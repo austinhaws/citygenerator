291fdcd82a4c3a79685f8a23ca1a688bf7e642dc
Austin Haws <ahaws@utah.gov>
2017-05-19 19:09:49 -0600

    Change google analytics to strategery

--- generate.php
+++ generate.php
@@ -169,15 +169,16 @@
 <?	include('template_bottom.inc');	?>
 <script>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-34431316-1']);
-  _gaq.push(['_setDomainName', 'crystalballsoft.com']);
-  _gaq.push(['_trackPageview']);
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-15960643-1']);
+	_gaq.push(['_setDomainName', 'strategerygames.com']);
+	_gaq.push(['_setAllowLinker', true]);
+	_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 
 </script>
